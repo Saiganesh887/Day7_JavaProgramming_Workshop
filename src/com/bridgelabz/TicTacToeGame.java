41b81fa4ad82722d868ad715e8cd174b8348854b
Your Name <you@example.com>
2023-01-02 06:47:14 +0530

    Player input the letter x 0r O

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -1,12 +1,27 @@
 package com.bridgelabz;
+
+import java.util.Scanner;
+
 public class TicTacToeGame {
     static char[] board = new char[10];
+    static char playerLetter;
+    static char computerLetter;
     static void createEmptyBoard(){
         for(int i = 1; i < board.length; i++){
             board[i] = ' ';
         }
     }
+    static void chooseInputLetter(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose a letter for you ( X  or  O ) to play game : ");
+        playerLetter = sc.next().toUpperCase().charAt(0);
+        if(playerLetter == 'X')
+            computerLetter = 'O';
+        if(playerLetter == 'O')
+            computerLetter = 'X';
+    }
     public static void main(String[] args) {
         createEmptyBoard();
+        chooseInputLetter();
     }
 }
