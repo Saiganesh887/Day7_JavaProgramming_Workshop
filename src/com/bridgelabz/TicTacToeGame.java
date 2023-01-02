c6f26576e1e481fc649d823ab01b396604d99449
Your Name <you@example.com>
2023-01-02 06:52:53 +0530

    show board to player

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -20,8 +20,16 @@ public class TicTacToeGame {
         if(playerLetter == 'O')
             computerLetter = 'X';
     }
+    static void showBoard(){
+        System.out.println(board[1] +" | "+ board[2] + " | " + board[3]);
+        System.out.println("----------");
+        System.out.println(board[4] +" | "+ board[5] + " | " + board[6]);
+        System.out.println("----------");
+        System.out.println(board[7] +" | "+ board[8] + " | " + board[9]);
+    }
     public static void main(String[] args) {
         createEmptyBoard();
         chooseInputLetter();
+        showBoard();
     }
 }
